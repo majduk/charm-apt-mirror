098efdf6eb5da6a133f6f00653965e4e06976298
Chi Wai, Chan <chiwai.chan@canonical.com>
2022-10-20 03:19:34 +0000

    Added some commonly used properties and helper functions to avoid code repetition.

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,8 +1,25 @@
+import asyncio
 import logging
 
 import pytest
 
 log = logging.getLogger(__name__)
+
+
+class Helper:
+    """Helper class for async functions."""
+
+    @staticmethod
+    async def run_action_wait(unit, action_name, **kwargs):
+        action = await unit.run_action(action_name, **kwargs)
+        await action.wait()
+        return action.results
+
+    @staticmethod
+    async def run_wait(unit, command):
+        action = await unit.run(command)
+        await action.wait()
+        return action.results
 
 
 def pytest_addoption(parser):
@@ -17,3 +34,28 @@ def pytest_addoption(parser):
 @pytest.fixture
 def series(request):
     return request.config.getoption("--series")
+
+
+@pytest.fixture(scope="class")
+def apt_mirror_app(ops_test):
+    return ops_test.model.applications["apt-mirror"]
+
+
+@pytest.fixture(scope="class")
+def apt_mirror_unit(apt_mirror_app):
+    return apt_mirror_app.units[0]
+
+
+@pytest.fixture(scope="class")
+def configs(apt_mirror_app):
+    async def get_config_synced():
+        return await apt_mirror_app.get_config()
+
+    loop = asyncio.get_event_loop()
+    coroutine = get_config_synced()
+    return loop.run_until_complete(coroutine)
+
+
+@pytest.fixture(scope="class")
+def helper():
+    return Helper
